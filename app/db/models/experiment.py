d14295c688d243bf9bb61ec1cc94aa13bf76353e
vladimir <vladimir.ivanov1307@gmail.com>
2026-03-13 11:00:01 +0300

    improved experiment model

--- app/db/models/experiment.py
+++ app/db/models/experiment.py
@@ -3,8 +3,8 @@ import uuid
 
 from typing import TYPE_CHECKING
 
-from sqlalchemy import DateTime, ForeignKey, JSON, String, Text
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Boolean, DateTime, ForeignKey, JSON, String, Text
+from sqlalchemy.dialects.postgresql import JSONB, UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 
@@ -17,18 +17,20 @@ class Experiment(Base):
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     experiment_number: Mapped[str] = mapped_column(String(10), nullable=False, unique=True, index=True)
     performed_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, index=True)
-    is_completed: Mapped[bool] = mapped_column(default=False, nullable=False)
+    is_completed: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     project: Mapped[str | None] = mapped_column(String(255))
     goal: Mapped[str] = mapped_column(Text, nullable=False)
     experiment_subject: Mapped[str | None] = mapped_column(String(255))
     perovskite: Mapped[str | None] = mapped_column(String(255))
-    samples: Mapped[list[str] | None] = mapped_column(JSON)
+    samples: Mapped[list[str] | None] = mapped_column(JSONB)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         server_default=func.now(),
+        nullable=False,
     )
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         server_default=func.now(),
-        onupdate=True,
+        onupdate=func.now(),
+        nullable=False,
     )
