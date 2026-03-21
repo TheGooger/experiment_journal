b8c85a8771d03d16e5306d5c5837f5f51c6704c6
vladimir <vladimir.ivanov1307@gmail.com>
2026-03-21 16:56:04 +0300

    add post option in experiment router

--- app/api/routes/experiment.py
+++ app/api/routes/experiment.py
@@ -1,12 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.schemas.experiment import Experiment
-from app.db.data import experiment_list
+from app.db.session import get_db
+from app.schemas.experiment import ExperimentCreate, ExperimentRead
+from app.services.experiment import ExperimentService
 
 
 router = APIRouter(prefix="/experiments", tags=["experiments"])
 
+service = ExperimentService()
 
-@router.get("/")
-async def get_all_experiments():
-    return experiment_list
+
+# @router.get("/")
+# async def get_all_experiments() -> list[ExperimentRead]:
+#     return experiment_list
+
+
+@router.post(
+        "/",
+        status_code=status.HTTP_201_CREATED,
+        response_model=ExperimentRead,
+)
+async def create_experiment(
+    data: ExperimentCreate,
+    db: AsyncSession = Depends(get_db),
+):
+    experiment = await service.create_experiment(db, data)
+
+    return experiment
+ 
