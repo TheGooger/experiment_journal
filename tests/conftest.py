8c2d91546d3fe52fcdfd2cee2b6f0d08db5b6ab6
vladimir <vladimir.ivanov1307@gmail.com>
2026-03-26 14:49:29 +0300

    add pytest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 import asyncio
+import os
+from dotenv import load_dotenv
 from typing import AsyncGenerator, Generator
 from fastapi.testclient import TestClient
 
@@ -11,8 +13,14 @@ from app.db.session import get_db
 from main import app
 
 
+load_dotenv()
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
+POSTGRES_DB = "test_db"
 # Тестовая БД
-DATABASE_URL = "postgresql+asyncpg://user:pass@localhost/test_db"
+DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 
 @pytest.fixture(scope="session")
