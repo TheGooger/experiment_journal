edd5c54c5c75299ee11d7e70b9947e7abd26b39b
vladimir <vladimir.ivanov1307@gmail.com>
2026-03-31 15:51:31 +0300

    add user check in experiment service

--- app/services/experiment.py
+++ app/services/experiment.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.repositories.experiment_repository import ExperimentRepository
 from app.schemas.experiment import ExperimentCreate, ExperimentUpdate
 from app.db.models.experiment import Experiment
+from app.db.models.user import User
 from app.core.exceptions import ExperimentNotFound
 
 
@@ -17,8 +18,9 @@ class ExperimentService:
             self,
             db: AsyncSession,
             data: ExperimentCreate,
+            user: User,
     ) -> Experiment:
-        experiment = await self.repository.create(db, data)
+        experiment = await self.repository.create(db, data, user.id)
 
         return experiment
     
@@ -26,9 +28,10 @@ class ExperimentService:
     async def read_all_experiments(
             self,
             db: AsyncSession,
+            user: User,
     ) -> Sequence[Experiment]:
         
-        experiment_list = await self.repository.read_all(db)
+        experiment_list = await self.repository.read_all(db, user.id)
 
         return experiment_list
     
@@ -37,9 +40,10 @@ class ExperimentService:
             self,
             db: AsyncSession,
             experiment_number: str,
+            user: User,
     ) -> Optional[Experiment]:
         
-        experiment = await self.repository.read_one(db, experiment_number)
+        experiment = await self.repository.read_one(db, experiment_number, user.id)
 
         if experiment is None:
             raise ExperimentNotFound(f"Experiment with number {experiment_number} not found")
@@ -52,9 +56,10 @@ class ExperimentService:
             db: AsyncSession,
             data: ExperimentUpdate,
             experiment_number: str,
+            user: User,
     ) -> Optional[Experiment]:
         
-        experiment = await self.repository.update(db, data, experiment_number)
+        experiment = await self.repository.update(db, data, experiment_number, user.id)
         if experiment is None:
             raise ExperimentNotFound(f"Experiment with number {experiment_number} not found")
         return experiment
@@ -64,9 +69,10 @@ class ExperimentService:
             self,
             db: AsyncSession,
             experiment_number: str,
+            user: User,
     ):
         
-        result = await self.repository.delete(db, experiment_number)
+        result = await self.repository.delete(db, experiment_number, user.id)
         if not result:
             raise ExperimentNotFound(f"Experiment with number {experiment_number} not found")
             
