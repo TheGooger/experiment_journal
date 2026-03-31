b0ba49802756f80f67fdabd16a69305500c70477
vladimir <vladimir.ivanov1307@gmail.com>
2026-03-31 17:40:38 +0300

    add tests with authorization

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,7 +7,7 @@ from app.schemas.experiment import ExperimentCreate
 pytestmark = pytest.mark.asyncio
 
 
-async def test_create_experiment_api(async_client: AsyncClient):
+async def test_create_experiment_api(async_client: AsyncClient, auth_headers):
     payload = {
         "experiment_number": "v260307",
         "performed_at": "2026-03-26",
@@ -15,7 +15,7 @@ async def test_create_experiment_api(async_client: AsyncClient):
         "project": "Test project",
         "goal": "Test goal",
     }
-    response = await async_client.post("/experiments/", json=payload)
+    response = await async_client.post("/experiments/", headers=auth_headers, json=payload)
     
     assert response.status_code == 201
     
@@ -26,13 +26,13 @@ async def test_create_experiment_api(async_client: AsyncClient):
     assert "id" in data
 
 
-async def test_get_one_experiment_not_found_api(async_client: AsyncClient):
-    response = await async_client.get("/experiments/NONEXISTENT")
+async def test_get_one_experiment_not_found_api(async_client: AsyncClient, auth_headers):
+    response = await async_client.get("/experiments/NONEXISTENT", headers=auth_headers)
     assert response.status_code == 404
     assert "not found" in response.json()["detail"].lower()
 
 
-async def test_update_experiment_api(async_client: AsyncClient):
+async def test_update_experiment_api(async_client: AsyncClient, auth_headers):
     create_payload = {
         "experiment_number": "v260310",
         "performed_at": "2026-03-26",
@@ -40,12 +40,12 @@ async def test_update_experiment_api(async_client: AsyncClient):
         "project": "Test project",
         "goal": "Test goal",
     }
-    create_resp = await async_client.post("/experiments/", json=create_payload)
+    create_resp = await async_client.post("/experiments/", headers=auth_headers, json=create_payload)
     assert create_resp.status_code == 201
     created_id = create_resp.json()['id']
 
     update_payload = {"project": "New test project"}
-    response = await async_client.patch("/experiments/v260310", json=update_payload)
+    response = await async_client.patch("/experiments/v260310", headers=auth_headers, json=update_payload)
     assert response.status_code == 200
     data = response.json()
     assert data["project"] == "New test project"
@@ -54,7 +54,7 @@ async def test_update_experiment_api(async_client: AsyncClient):
     assert data['id'] == created_id
 
 
-async def test_delete_experiment_api(async_client: AsyncClient):
+async def test_delete_experiment_api(async_client: AsyncClient, auth_headers):
     create_payload = {
         "experiment_number": "v260312",
         "performed_at": "2026-03-26",
@@ -62,11 +62,11 @@ async def test_delete_experiment_api(async_client: AsyncClient):
         "project": "Test project",
         "goal": "To delete",
     }
-    await async_client.post("/experiments/", json=create_payload)
+    await async_client.post("/experiments/", headers=auth_headers, json=create_payload)
 
-    delete_resp = await async_client.delete("/experiments/v260312")
+    delete_resp = await async_client.delete("/experiments/v260312", headers=auth_headers)
     assert delete_resp.status_code == 204
     
-    get_resp = await async_client.get("/experiments/v260312")
+    get_resp = await async_client.get("/experiments/v260312", headers=auth_headers)
     assert get_resp.status_code == 404
     
--- tests/test_repository.py
+++ tests/test_repository.py
@@ -9,7 +9,7 @@ from app.schemas.experiment import ExperimentCreate, ExperimentUpdate
 pytestmark = pytest.mark.asyncio
 
 
-async def test_create_experiment(db_session):
+async def test_create_experiment(db_session, test_user):
     repo = ExperimentRepository()
     data = ExperimentCreate(
     experiment_number = "v260305",
@@ -22,20 +22,20 @@ async def test_create_experiment(db_session):
     samples = ["1", "2", "3"],
     )
 
-    experiment = await repo.create(db_session, data)
+    experiment = await repo.create(db_session, data, test_user.id)
     
     assert experiment.id is not None
     assert experiment.experiment_number == "v260305"
     
 
-async def test_read_one_not_found(db_session):
+async def test_read_one_not_found(db_session, test_user):
     repo = ExperimentRepository()
-    result = await repo.read_one(db_session, "123")
+    result = await repo.read_one(db_session, "123", test_user.id)
 
     assert result is None
 
 
-async def test_read_one(db_session):
+async def test_read_one(db_session, test_user):
     repo = ExperimentRepository()
     data = ExperimentCreate(
     experiment_number = "v260306",
@@ -47,14 +47,14 @@ async def test_read_one(db_session):
     perovskite = "MAPbBr2I",
     samples = ["1", "2", "3"],
     )
-    created = await repo.create(db_session, data)
-    found = await repo.read_one(db_session, "v260306")
+    created = await repo.create(db_session, data, test_user.id)
+    found = await repo.read_one(db_session, "v260306", test_user.id)
 
     assert found is not None
     assert found.id == created.id
 
 
-async def test_update_experiment(db_session):
+async def test_update_experiment(db_session, test_user):
     repo = ExperimentRepository()
     data = ExperimentCreate(
     experiment_number = "v260307",
@@ -66,28 +66,28 @@ async def test_update_experiment(db_session):
     perovskite = "MAPbBr2I",
     samples = ["1", "2", "3"],
     )
-    created = await repo.create(db_session, data)
+    created = await repo.create(db_session, data, test_user.id)
 
     update_data = ExperimentUpdate(
         is_completed=True,
     ) 
-    updated = await repo.update(db_session, update_data, created.experiment_number)
+    updated = await repo.update(db_session, update_data, created.experiment_number, test_user.id)
 
     assert updated is not None
     assert updated.is_completed == True
 
 
-async def test_update_experiment_not_found(db_session):
+async def test_update_experiment_not_found(db_session, test_user):
     repo = ExperimentRepository()
     update_data = ExperimentUpdate(
         is_completed=False,
     )
-    updated = await repo.update(db_session, update_data, "123")
+    updated = await repo.update(db_session, update_data, "123", test_user.id)
 
     assert updated is None
 
 
-async def test_delete_experiment(db_session):
+async def test_delete_experiment(db_session, test_user):
     repo = ExperimentRepository()
     data = ExperimentCreate(
     experiment_number = "v260308",
@@ -99,19 +99,19 @@ async def test_delete_experiment(db_session):
     perovskite = "MAPbBr2I",
     samples = ["1", "2", "3"],
     )
-    created = await repo.create(db_session, data)
+    created = await repo.create(db_session, data, test_user.id)
     
-    deleted = await repo.delete(db_session, created.experiment_number)
+    deleted = await repo.delete(db_session, created.experiment_number, test_user.id)
 
     assert deleted == True
 
-    found = await repo.read_one(db_session, created.experiment_number)
+    found = await repo.read_one(db_session, created.experiment_number, test_user.id)
     assert found is None
 
 
-async def test_delete_experiment_not_found(db_session):
+async def test_delete_experiment_not_found(db_session, test_user):
     repo = ExperimentRepository()
 
-    deleted = await repo.delete(db_session, "123")
+    deleted = await repo.delete(db_session, "123", test_user.id)
 
     assert deleted == False
