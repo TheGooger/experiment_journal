f53c270bbd721a7ef844ba1d164b8ca55ca63746
vladimir <vladimir.ivanov1307@gmail.com>
2026-03-31 15:53:33 +0300

    add access token decoder

--- app/core/security.py
+++ app/core/security.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-from jose import jwt
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 
 from app.core.config import settings
@@ -29,3 +29,15 @@ def create_access_token(data: dict) -> str:
     to_encode.update({"exp": expire})
 
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str) -> dict | None:
+    try:
+        payload = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM],
+        )
+        return payload
+    except JWTError:
+        return None
+    
